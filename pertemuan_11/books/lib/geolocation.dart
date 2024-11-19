cd874e25313d452fa3eb97b7b8d81edba627a08b
NadilaAmaliaaa <nadilaamaliapribadi@gmail.com>
2024-11-19 14:29:24 +0700

    W11:Soal 13

--- pertemuan_11/books/lib/geolocation.dart
+++ pertemuan_11/books/lib/geolocation.dart
@@ -10,15 +10,19 @@ class LocationScreen extends StatefulWidget {
 
 class _LocationScreenState extends State<LocationScreen> {
   String myPosition = '';
+  Future<Position>? position;
+
   @override
   void initState() {
+    // super.initState();
+    // getPosition().then((Position myPos){
+    //   myPosition = 'Latitude: ${myPos.latitude.toString()}, Longitude: ${myPos.longitude.toString()}';
+    //   setState(() {
+    //     myPosition = myPosition;
+    //   });
+    // });
     super.initState();
-    getPosition().then((Position myPos){
-      myPosition = 'Latitude: ${myPos.latitude.toString()}, Longitude: ${myPos.longitude.toString()}';
-      setState(() {
-        myPosition = myPosition;
-      });
-    });
+    position = getPosition();
   }
 
   @override
@@ -33,16 +37,32 @@ class _LocationScreenState extends State<LocationScreen> {
   //   );
   // }
     Widget build(BuildContext context){
-    final myWidget = myPosition == ''
-    ? const CircularProgressIndicator()
-    : Text(myPosition);
+    // final myWidget = myPosition == ''
+    // ? const CircularProgressIndicator()
+    // : Text(myPosition);
+    // return Scaffold(
+    //   appBar: AppBar(
+    //     title: const Text('Current Location Nadila'),
+    //   ),
+    //   body: Center(
+    //     child: myWidget,
+    //   ),
+    // );
+
+    //Praktikum 7
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Current Location Nadila'),
-      ),
-      body: Center(
-        child: myWidget,
-      ),
+      appBar: AppBar(title: Text('Current Location Nadila'),),
+      body: Center(child: FutureBuilder(
+        future: position, 
+        builder: (BuildContext context, AsyncSnapshot<Position> snapshot){
+          if(snapshot.connectionState==ConnectionState.waiting){
+            return const CircularProgressIndicator();
+          }else if(snapshot.connectionState==ConnectionState.done){
+            return Text(snapshot.data.toString());
+          }else{
+            return const Text('');
+          }
+        }),),
     );
   }
   Future<Position> getPosition() async {
