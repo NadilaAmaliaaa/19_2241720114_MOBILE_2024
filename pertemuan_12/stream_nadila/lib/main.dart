b8f4720db99aec4613e4b107797835b34a102be3
NadilaAmaliaaa <nadilaamaliapribadi@gmail.com>
2024-11-24 14:05:10 +0700

    W12: Jawaban Soal 6

--- pertemuan_12/stream_nadila/lib/main.dart
+++ pertemuan_12/stream_nadila/lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'stream.dart';
+import 'dart:async';
+import 'dart:math';
 
 void main() {
   runApp(const MyApp());
@@ -15,13 +17,13 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.deepPurple,
       ),
-      home: StreamHomePage(),
+      home: const StreamHomePage(),
     );
   }
 }
 
 class StreamHomePage extends StatefulWidget {
-  StreamHomePage({super.key});
+  const StreamHomePage({super.key});
 
   @override
   State<StreamHomePage> createState() => _StreamHomePageState();
@@ -30,6 +32,10 @@ class StreamHomePage extends StatefulWidget {
 class _StreamHomePageState extends State<StreamHomePage> {
   Color bgColor = Colors.blueGrey;
   late ColorStream colorStream;
+
+  int lastNumber = 0;
+  late StreamController numberStreamController;
+  late NumberStream numberStream;
 
   void changeColor() async{
     // SOAL 4
@@ -46,11 +52,32 @@ class _StreamHomePageState extends State<StreamHomePage> {
     });
   }
 
+  void addRandomNumber() {
+    Random random = Random();
+    int myNum = random.nextInt(10);
+    numberStream.addNumberToSink(myNum);
+  }
+
   @override
   void initState() {
-    super.initState();
-    colorStream = ColorStream();
-    changeColor();
+    // super.initState();
+    // colorStream = ColorStream();
+    // changeColor();
+
+    numberStream = NumberStream();
+    numberStreamController = numberStream.controller;
+    Stream stream = numberStreamController.stream;
+    stream.listen((event){
+      setState(() {
+        lastNumber = event;
+      });
+    });
+  }
+
+  @override
+  void dispose() {
+    numberStreamController.close();
+    super.dispose();
   }
 
   @override
@@ -59,8 +86,23 @@ class _StreamHomePageState extends State<StreamHomePage> {
       appBar: AppBar(
         title: const Text('Stream Nadila'),
       ),
-      body: Container(
-        decoration: BoxDecoration(color: bgColor),
+      body: 
+      // Container(
+      //   decoration: BoxDecoration(color: bgColor),
+      // )
+      SizedBox(
+        width: double.infinity,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Text(lastNumber.toString()),
+            ElevatedButton(
+              onPressed: ()=>addRandomNumber(),
+              child: const Text('New Random Number'),
+            )
+          ],
+        ),
       )
     );
   }
